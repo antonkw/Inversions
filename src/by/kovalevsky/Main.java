026b3e4a6debe5a183b926bbccba9cbf52ffdf62
antonkw <antonkw.mail@gmail.com>
2015-07-13 23:21:35 +0300

    update main

--- src/by/kovalevsky/Main.java
+++ src/by/kovalevsky/Main.java
@@ -1,8 +1,15 @@
 package by.kovalevsky;
 
+import org.junit.runner.JUnitCore;
+import tests.CountInversionsTest;
+
 public class Main {
 
     public static void main(String[] args) {
+        JUnitCore junit = new JUnitCore();
+        StringBuilder finalResult = new StringBuilder("Counts of merge sort test`s fails: ");
+        finalResult.append(junit.run(CountInversionsTest.class).getFailureCount());
+        System.out.print(finalResult);
 	// write your code here
     }
 }
